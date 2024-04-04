f0fda5cb895fc681b31e23bb15238f713c5923c6
Martyna Zawór <mart.zaw.95@gmail.com>
2024-04-04 21:41:17 +0200

    Organize code

--- bank.py
+++ bank.py
@@ -1,6 +1,8 @@
-import json, csv, requests
+import csv
+import requests
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 def get_data():
     response = requests.get("https://api.nbp.pl/api/exchangerates/tables/C?format=json")
@@ -13,31 +15,27 @@ def get_data():
     for i in rates:
         currency_dict[i['currency']] = i['ask']
 
-    
     with open('bank.csv', 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=fields, delimiter=';')
         writer.writeheader()
         writer.writerows(rates)
-       
-     
+
     return currency_dict
 
 
-@app.route('/converter', methods=['GET','POST'])
+@app.route('/converter', methods=['GET', 'POST'])
 def converter():
     currency_dict = get_data()
     if request.method == 'POST':
         currency = request.form['currency']
         amount = float(request.form['amount'])
         currency_ask = currency_dict.get(currency)
-        if currency_ask:      
+        if currency_ask:
             converted_amount = round(float(currency_ask) * amount, 2)
             return render_template('converter.html', converted_amount=converted_amount, currencies=currency_dict.keys(), currency=currency, amount=amount)
     else:
         return render_template('converter.html', currencies=currency_dict.keys())
 
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
-
